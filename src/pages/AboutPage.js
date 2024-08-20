b3e793afb53883961a8cec0beedd04e0ac2f8e8a
Nikola Kenjic <nikola.nico89@gmail.com>
2024-08-20 20:21:01 +0200

    fixed

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -6,11 +6,11 @@ const About = () => {
     <div className={classes.about}>
       <h1>Movie App</h1>
       <p>
-        Welcome to Movie App - your cinematic companion for effortless movie
+        Welcome to Movie App-your cinematic companion for effortless movie
         exploration. We're dedicated to revolutionizing the way you discover and
-        enjoy films, tailored precisely to your preferences. Our Focus At Movie
-        App, our mission is clear: to simplify your movie search while enhancing
-        your movie night. We're not just an app, we're your movie matchmakers.
+        enjoy films, tailored precisely to your preferences.Our focus and
+        mission is clear: to simplify your movie search! We&apos;re not just an
+        app, we&apos;re your movie matchmakers.
       </p>
       <p>Thanks for choosing Movie App!!!</p>
     </div>
