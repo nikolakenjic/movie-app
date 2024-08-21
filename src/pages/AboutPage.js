a828921ad9e6aacdcb16a425d17978a6b8f0ba54
Nikola Kenjic <nikola.nico89@gmail.com>
2024-08-21 21:44:03 +0200

    about

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -8,7 +8,7 @@ const About = () => {
       <p>
         Welcome to Movie App-your cinematic companion for effortless movie
         exploration. We're dedicated to revolutionizing the way you discover and
-        enjoy films, tailored precisely to your preferences.Our focus and
+        enjoy films, tailored precisely to your preferences. Our focus and
         mission is clear: to simplify your movie search! We&apos;re not just an
         app, we&apos;re your movie matchmakers.
       </p>
